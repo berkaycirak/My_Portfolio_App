28f2eb5819cd97a980a40ed8f6534e5c45591822
Berkay <berkaycirak_11@hotmail.com>
2022-07-06 19:52:47 +0300

    Yarn and Navbar is added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
