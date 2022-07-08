ebee0d03896a59e4a8b645306d2d0a580e4b2dc6
Berkay ÇIRAK <berkay@192.168.1.42>
2022-07-08 23:39:48 +0300

    sass is added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/layout/Navbar';
+import Navbar from './components/layout/Navbar/Navbar';
 
 function App() {
   return (
---
+++ src/components/layout/Navbar/Navbar.jsx
@@ -0,0 +1,7 @@
+import "./Navbar.styles.scss"
+
+function Navbar() {
+  return <div className="navbar"> Navbar</div>;
+}
+
+export default Navbar;
